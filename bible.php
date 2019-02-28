bd4a50378a57eaf22aaee16d46ac469ec0e0033b
Rodolfo Godinez <rgodinez@pro-activemediadesigns.com>
2019-02-27 19:59:28 -0800

    Started working on the bible page creating all the menus and dividing the content areas.

--- bible.php
+++ bible.php
@@ -1,63 +1,131 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: papi
- * Date: 8/2/18
+ * Created by NetBeans.
+ * User: Rodolfo Godinez
+ * Date: 2/27/2019
  * Time: 3:13 PM
  */
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <!-- Required meta tags -->
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">-->
-    <link rel="stylesheet" href="css/bootstrap4/bootstrap.css" />
-    <link rel="stylesheet" href="css/bootstrap4/bootstrap-reboot.css" />
+        <!-- Bootstrap CSS -->
+        <!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">-->
+        <link rel="stylesheet" href="css/bootstrap4/bootstrap.css"/>
+        <link rel="stylesheet" href="css/bootstrap4/bootstrap-reboot.css"/>
 
-    <title>Jesus Last Days - Bible</title>
+        <title>Jesus Last Days - Bible</title>
 
-    <!-- General Styles -->
-    <link rel="stylesheet" href="css/mainCSS.css" />
-    <link rel="stylesheet" href="css/mapStyles.css" />
+        <!-- General Styles -->
+        <link rel="stylesheet" href="css/mainCSS.css"/>
+        <link rel="stylesheet" href="css/mapStyles.css"/>
+        <link rel="stylesheet" href="css/lastDaysStyles.css"/>
 
-    <!-- MapBox -->
-    <script src='https://api.mapbox.com/mapbox-gl-js/v0.47.0/mapbox-gl.js'></script>
-    <link href='https://api.mapbox.com/mapbox-gl-js/v0.47.0/mapbox-gl.css' rel='stylesheet' />
+        <!-- MapBox -->
+        <script src='https://api.mapbox.com/mapbox-gl-js/v0.47.0/mapbox-gl.js'></script>
+        <link href='https://api.mapbox.com/mapbox-gl-js/v0.47.0/mapbox-gl.css' rel='stylesheet'/>
 
-</head>
-<body>
-<div id="headerMain" class="container-fluid">
-    <nav class="navbar navbar-expand-lg bg-light">
-        <span class="navbar-brand mb-0 h1 float-left">Last Days</span>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarMain">
-            <div class="navbar-nav float-right">
-                <a class="nav-item nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a>
-                <a class="nav-link" href="lastDays.php">Last Days of Jesus</a>
-                <a class="nav-link" href="bible.php">Bible</a>
-                <a class="nav-link" href="prophecies.php">Prophecies</a>
-                <a class="nav-link" href="jesusTravels.php">Jesus Travels</a>
-                <a class="nav-link" href="gallery.php">Gallery</a>
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<!--        <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>-->
 
-            </div>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">SEARCH</button>
-            </form>
+
+    </head>
+    <body>
+        <div id="headerMain" class="container-fluid bg-light">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">Last Days</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="lastDays.php">Last Days of Jesus</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="bible.php">Bible <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="prophecies.php">Prophecies</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="jesusTravels.php">Jesus Travels</a><!-- Maps showing Jesus's travel routes -->
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="gallery.php">Gallery</a><!-- Gallery of images and artifacts -->
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
-    </nav>
-</div>
+
+        <div id="bibleContent" class="container container-fluid">
+            <div class="row">
+                <div class="col-6">
+                    <div class="row">
+                        <h3>Bible Books</h3>
+                    </div>
+                    <div class="row">
+                        <div class="col-4"> <!-- Column for book buttons -->
+                            <div class="list-group">
+                                <button id="genesisBtn" type="button" class="list-group-item list-group-item-action">Genesis</button>
+                                <button id="exodusBtn" type="button" class="list-group-item list-group-item-action">Exodus</button>
+                                <button id="leviticusBtn" type="button" class="list-group-item list-group-item-action">Leviticus</button>
+                                <button id="numbersBtn" type="button" class="list-group-item list-group-item-action">Numbers</button>
+                                <button id="deuteronomyBtn" type="button" class="list-group-item list-group-item-action">Deuteronomy</button>
+                                <button id="joshuaBtn" type="button" class="list-group-item list-group-item-action">Joshua</button>
+                                <button id="judgesBtn" type="button" class="list-group-item list-group-item-action">Judges</button>
+                                <button id="ruthBtn" type="button" class="list-group-item list-group-item-action">Ruth</button>
+                                <button id="firstSamuelBtn" type="button" class="list-group-item list-group-item-action">1 Samuel</button>
+                                <button id="secondSamuelBtn" type="button" class="list-group-item list-group-item-action">1 Samuel</button>
+                                <button id="firstKingsBtn" type="button" class="list-group-item list-group-item-action">1 Kings</button>
+                                <button id="secondKingsBtn" type="button" class="list-group-item list-group-item-action">2 Kings</button>
+                                <button id="firstChroniclesBtn" type="button" class="list-group-item list-group-item-action">1 Chronicles</button>
+                                <button id="secondChroniclesBtn" type="button" class="list-group-item list-group-item-action">2 Chronicles</button>
+                                <button id="ezraBtn" type="button" class="list-group-item list-group-item-action">Ezra</button>
+                                <button id="nehemiahBtn" type="button" class="list-group-item list-group-item-action">Nehemiah</button>
+                                <button id="estherBtn" type="button" class="list-group-item list-group-item-action">Esther</button>
+                                <button id="jobBtn" type="button" class="list-group-item list-group-item-action">Job</button>
+                                <button id="psalmsBtn" type="button" class="list-group-item list-group-item-action">Psalms</button>
+                                <button id="proverbsBtn" type="button" class="list-group-item list-group-item-action">Proverbs</button>
+                                <button id="ecclesiastesBtn" type="button" class="list-group-item list-group-item-action">Ecclesiastes</button>
+                                <button id="songOfSolBtn" type="button" class="list-group-item list-group-item-action">Song of Solomon</button>
+                                <button id="genesisBtn" type="button" class="list-group-item list-group-item-action">Genesis</button>
+                                <button id="genesisBtn" type="button" class="list-group-item list-group-item-action">Genesis</button>
+                                <button id="genesisBtn" type="button" class="list-group-item list-group-item-action">Genesis</button>
+                                <button id="genesisBtn" type="button" class="list-group-item list-group-item-action">Genesis</button>
+                                <button id="genesisBtn" type="button" class="list-group-item list-group-item-action">Genesis</button>
+                                <button id="genesisBtn" type="button" class="list-group-item list-group-item-action">Genesis</button>
+                                <button id="genesisBtn" type="button" class="list-group-item list-group-item-action">Genesis</button>
+                                <button id="genesisBtn" type="button" class="list-group-item list-group-item-action">Genesis</button>
+                            </div>
+                        </div>
+                        <div class="col-8"> <!-- Column for book submenu --> 
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6">
+
+                </div>
+            </div>
+        </div>
 
 
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-</body>
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </body>
 </html>
--- index.php
+++ index.php
@@ -5,91 +5,97 @@
  * Date: 7/24/2018
  * Time: 5:23 PM
  */
-
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <!-- Required meta tags -->
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">-->
-    <link rel="stylesheet" href="css/bootstrap4/bootstrap.css"/>
-    <link rel="stylesheet" href="css/bootstrap4/bootstrap-reboot.css"/>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <!--<link rel="stylesheet" href="css/bootstrap4/bootstrap.css"/>-->
+        <!--<link rel="stylesheet" href="css/bootstrap4/bootstrap-reboot.css"/>-->
 
-    <title>Jesus Last Days - Home</title>
+        <title>Jesus Last Days - Home</title>
 
-    <!-- General Styles -->
-    <link rel="stylesheet" href="css/mainCSS.css"/>
-    <link rel="stylesheet" href="css/mapStyles.css"/>
-    <link rel="stylesheet" href="css/homeStyles.css" />
+        <!-- General Styles -->
+        <link rel="stylesheet" href="css/mainCSS.css"/>
+        <link rel="stylesheet" href="css/mapStyles.css"/>
+        <link rel="stylesheet" href="css/homeStyles.css" />
 
-    <!-- MapBox -->
-    <script src='https://api.mapbox.com/mapbox-gl-js/v0.47.0/mapbox-gl.js'></script>
-    <link href='https://api.mapbox.com/mapbox-gl-js/v0.47.0/mapbox-gl.css' rel='stylesheet'/>
+        <!-- MapBox -->
+        <script src='https://api.mapbox.com/mapbox-gl-js/v0.47.0/mapbox-gl.js'></script>
+        <link href='https://api.mapbox.com/mapbox-gl-js/v0.47.0/mapbox-gl.css' rel='stylesheet'/>
 
-</head>
-<body>
-<div id="headerMain" class="container-fluid">
-    <nav class="navbar navbar-expand-lg bg-light">
-        <span class="navbar-brand mb-0 h1 float-left">Last Days</span>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarMain">
-            <div class="navbar-nav float-right">
-                <a class="nav-item nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a>
-                <a class="nav-link" href="lastDays.php">Last Days of Jesus</a>
-                <a class="nav-link" href="bible.php">Bible</a>
-                <a class="nav-link" href="prophecies.php">Prophecies</a>
-                <a class="nav-link" href="jesusTravels.php">Jesus Travels</a>
-                <a class="nav-link" href="gallery.php">Gallery</a>
+    </head>
+    <body>
+        <div id="headerMain" class="container-fluid bg-light">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">Last Days</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="lastDays.php">Last Days of Jesus</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="bible.php">Bible</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="prophecies.php">Prophecies</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="jesusTravels.php">Jesus Travels</a><!-- Maps showing Jesus's travel routes -->
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="gallery.php">Gallery</a><!-- Gallery of images and artifacts -->
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
 
+        <div id="homeContent" class="container-fluid">
+            <div class="row">
+                <h2>The Last Days of Jesus Christ</h2>
+                <!-- MapBox Start -->
+                <div id='mapHome' style="width: 75%; height: calc(100vh - 100px);"></div>
             </div>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">SEARCH</button>
-            </form>
+            <div class="row fixed-bottom">
+                <div id="timeLineHome" class="col-12">
+                    <h4>TIMELINE</h4>
+                </div>
+            </div>
+
         </div>
-    </nav>
-</div>
-
-<div id="homeContent" class="container-fluid">
-    <div class="row">
-        <h2>The Last Days of Jesus Christ</h2>
-        <!-- MapBox Start -->
-        <div id='mapHome' style="width: 75%; height: 600px;"></div>
-    </div>
-    <div class="row">
-        <div id="timeLineHome" class="col-12">
-            <h4>TIMELINE</h4>
-        </div>
-    </div>
-
-</div>
-<script>
-    mapboxgl.accessToken = 'pk.eyJ1IjoiZ2FuZ3VzY2F0IiwiYSI6ImNqazBoM29sOTA2c2wzd29nNzZqYnlla24ifQ.jzOzrlUH8Kx9vya3R-ZMqQ';
-    var map = new mapboxgl.Map({
-        container: 'mapHome',
-        style: 'mapbox://styles/ganguscat/cjkd03ki99c7u2rpbt43iommv',
-        center: [35.217018, 31.771959],
-        zoom: 9
-    });
-</script>
-<!-- MapBox End -->
+        <script>
+            mapboxgl.accessToken = 'pk.eyJ1IjoiZ2FuZ3VzY2F0IiwiYSI6ImNqazBoM29sOTA2c2wzd29nNzZqYnlla24ifQ.jzOzrlUH8Kx9vya3R-ZMqQ';
+            var map = new mapboxgl.Map({
+                container: 'mapHome',
+                style: 'mapbox://styles/ganguscat/cjkd03ki99c7u2rpbt43iommv',
+                center: [35.217018, 31.771959],
+                zoom: 9
+            });
+            
+//            map.addControl(new mapboxgl.FullscreenControl()); //Adds full screen button for user control
+        </script>
+        <!-- MapBox End -->
 
 
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-        crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-        crossorigin="anonymous"></script>
-</body>
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </body>
 </html>
